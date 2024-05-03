60f45d5252a60e441e9416fc9a241eb3a1c1c8d7
Mohamed Sherif <mohamed.sheriff.mahdy@gmail.com>
2024-05-03 14:59:26 +0300

    Use Cshrper..

--- EmployeeManagement_API/Service/ServiceEmployee.cs
+++ EmployeeManagement_API/Service/ServiceEmployee.cs
@@ -4,55 +4,57 @@ using EmployeeManagement_API.Repository;
 
 namespace EmployeeManagement_API.Service
 {
-  public class ServiceEmployee : IServiceEmployee
-  {
-    private readonly IRepository<Employee> _repository;
-    private readonly IRepository<Job> _repositoryJob;
-    public ServiceEmployee(IRepository<Employee> repository, IRepository<Job> repositoryJob)
+    public class ServiceEmployee : IServiceEmployee
     {
-      _repository = repository;
-      _repositoryJob = repositoryJob;
+        private readonly IRepository<Employee> _repository;
+        private readonly IRepository<Job> _repositoryJob;
+
+        public ServiceEmployee(IRepository<Employee> repository, IRepository<Job> repositoryJob)
+        {
+            _repository = repository;
+            _repositoryJob = repositoryJob;
+        }
+
+        public void AddEmployee(Employee employee) => _repository.Insert(employee);
+
+        public void DeleteEmployee(Employee employee) => _repository.Delete(employee);
+
+        public bool EmployeeExists(int id) => _repository.Exists(e => e.EmployeeId == id);
+
+        public Employee EmployeeReMap(EmployeeWithJobTitleDto employeeWithJobTitleDto)
+        {
+            Employee employee = new Employee();
+            employee.EmployeeId = employeeWithJobTitleDto.EmployeeId;
+            employee.Name = employeeWithJobTitleDto.Name;
+            employee.Gender = employeeWithJobTitleDto.Gender;
+            employee.IsFirstAppointment = employeeWithJobTitleDto.IsFirstAppointment;
+            employee.StartDate = employeeWithJobTitleDto.StartDate;
+            employee.Notes = employeeWithJobTitleDto.Notes;
+            employee.JobId = _repositoryJob
+                .GetBy(j => j.JobTitle == employeeWithJobTitleDto.JobTitle)
+                .JobId;
+            return employee;
+        }
+
+        public EmployeeWithJobTitleDto EmployeeWithJobTitleDto(Employee employee)
+        {
+            EmployeeWithJobTitleDto employeeWithJobTitleDto = new EmployeeWithJobTitleDto();
+            employeeWithJobTitleDto.EmployeeId = employee.EmployeeId;
+            employeeWithJobTitleDto.Name = employee.Name;
+            employeeWithJobTitleDto.Gender = employee.Gender;
+            employeeWithJobTitleDto.IsFirstAppointment = employee.IsFirstAppointment;
+            employeeWithJobTitleDto.StartDate = employee.StartDate;
+            employeeWithJobTitleDto.Notes = employee.Notes;
+            employeeWithJobTitleDto.JobTitle = employee.Job.JobTitle;
+            return employeeWithJobTitleDto;
+        }
+
+        public Employee FitchEmployee(int id) =>
+            GetEmployeesWithjobs().FirstOrDefault(e => e.EmployeeId == id);
+
+        public List<Employee> GetEmployeesWithjobs() => _repository.GetWithInclude("Job");
+
+        public void UpdateEmployee(Employee DataBaseEmployee, Employee employee) =>
+            _repository.Update(DataBaseEmployee, employee);
     }
-    public void AddEmployee(Employee employee) => _repository.Insert(employee);
-
-    public void DeleteEmployee(Employee employee) => _repository.Delete(employee);
-
-    public bool EmployeeExists(int id) => _repository.Exists(e => e.EmployeeId == id);
-
-
-    public Employee EmployeeReMap(EmployeeWithJobTitleDto employeeWithJobTitleDto)
-    {
-      Employee employee = new Employee();
-      employee.EmployeeId = employeeWithJobTitleDto.EmployeeId;
-      employee.Name = employeeWithJobTitleDto.Name;
-      employee.Gender = employeeWithJobTitleDto.Gender;
-      employee.IsFirstAppointment = employeeWithJobTitleDto.IsFirstAppointment;
-      employee.StartDate = employeeWithJobTitleDto.StartDate;
-      employee.Notes = employeeWithJobTitleDto.Notes;
-      employee.JobId = _repositoryJob.GetBy(j => j.JobTitle == employeeWithJobTitleDto.JobTitle).JobId;
-      return employee;
-    }
-
-    public EmployeeWithJobTitleDto EmployeeWithJobTitleDto(Employee employee)
-    {
-      EmployeeWithJobTitleDto employeeWithJobTitleDto = new EmployeeWithJobTitleDto();
-      employeeWithJobTitleDto.EmployeeId = employee.EmployeeId;
-      employeeWithJobTitleDto.Name = employee.Name;
-      employeeWithJobTitleDto.Gender = employee.Gender;
-      employeeWithJobTitleDto.IsFirstAppointment = employee.IsFirstAppointment;
-      employeeWithJobTitleDto.StartDate = employee.StartDate;
-      employeeWithJobTitleDto.Notes = employee.Notes;
-      employeeWithJobTitleDto.JobTitle = employee.Job.JobTitle;
-      return employeeWithJobTitleDto;
-    }
-
-    public Employee FitchEmployee(int id) => GetEmployeesWithjobs().FirstOrDefault(e => e.EmployeeId == id);
-
-    public List<Employee> GetEmployeesWithjobs() => _repository.GetWithInclude("Job");
-
-
-    public void UpdateEmployee
-      (Employee DataBaseEmployee, Employee employee) => _repository.Update(DataBaseEmployee, employee);
-
-  }
 }
